7eda5382f967f49c6e8ef7a716b94e7e842fa336
Henry Lo <risenhell@gmail.com>
2014-10-09 12:34:58 -0700

    Just adding my beloved loading icon
    ^ that being said.

--- js/app.js
+++ js/app.js
@@ -10,14 +10,19 @@ var binding = function(itemHolder){
 	var link = itemHolder.querySelector("a");
 	$(link).click(function(){
 		console.log('running');
+		$('#mainContent').hide(500);
+		$('#loading').show(500);
 		var name = link.getAttribute("id") + ".php";
-		$('#mainContent').load(name);
+		$('#mainContent').load(name, function(){
+			$('#mainContent').show(500);
+			$('#loading').hide(500);
+		});
 	})
 }
-
 
 $(document).ready(function(){
 	for (var i = 0; i < sideBar.children.length	; i++){
 		binding(sideBar.children[i]);
 	}
+	$('#loading').hide(500);
 })
